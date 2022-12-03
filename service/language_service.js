c29d4baffbbc2e29339171621516d0ddaed0f5aa
elenapark <elenability@naver.com>
2022-12-03 19:48:51 +0900

    add : 추가구현2 - API 캐싱

--- service/language_service.js
+++ service/language_service.js
@@ -1,10 +1,20 @@
 import { API } from "../config/api.js";
 
+const apiStorage = {};
+
 export const getLanguages = async (keyword) => {
+  const url = `${API}/languages?keyword=${keyword}`;
+
+  if (apiStorage[url]) {
+    return apiStorage[url];
+  }
+
   try {
-    const res = await fetch(`${API}/languages?keyword=${keyword}`);
+    const res = await fetch(url);
     if (res.ok) {
-      return res.json();
+      const json = res.json();
+      apiStorage[url] = json;
+      return json;
     }
     return Promise.reject(new Error("API 응답에 실패했습니다."));
   } catch (err) {
